061c61aeee745119d94f72b2e43596eb4b58a837
Andrew Vo <andrewqv@uci.edu>
2018-02-03 21:36:18 -0800

    Added eaten foods for profile view controller

--- HackUCI2018/Controller/ProfileViewController.swift
+++ HackUCI2018/Controller/ProfileViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var totalCaloriesLabel: UILabel!
     @IBOutlet weak var caloriesConsumedLabel: UILabel!
     @IBOutlet weak var remainingCaloriesLabel: UILabel!
+    var stringOfEatenFoods = ""
+    var dailyCalorieGoal = 0
+    var consumedCalories = 0
+    var remainingCalories = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setStringOfEatenFoods()
+        setDailyCalorieGoal()
+        setConsumedCalories()
+        setRemainingCalories()
         // Do any additional setup after loading the view.
     }
-
+    
+    func setStringOfEatenFoods() {
+        let usersDB = Database.database().reference().child("Users")
+        guard let myUserID = Auth.auth().currentUser?.uid else {return}
+        let userNEW = usersDB.child(myUserID).child("EatenFood")
+        userNEW.observeSingleEvent(of: .value) { (snapshot) in
+            let snapshotValue = snapshot.value as! String
+            self.stringOfEatenFoods = snapshotValue
+            print(self.stringOfEatenFoods) //ACESS THE STRING WITH THIS
+            //WRITE CODE IN HERE
+            //WRITE IN HERE THIS IS IMPORTANT!!
+        }
+    }
+    
+    func setDailyCalorieGoal() {
+        let usersDB = Database.database().reference().child("Users")
+        guard let myUserID = Auth.auth().currentUser?.uid else {return}
+        let userNEW = usersDB.child(myUserID).child("TotalCalories")
+        userNEW.observeSingleEvent(of: .value) { (snapshot) in
+            let snapshotValue = snapshot.value as! Int
+            self.dailyCalorieGoal = snapshotValue
+            print(self.dailyCalorieGoal) //ACESS THE INT WITH THIS
+            //WRITE CODE IN HERE
+            self.totalCaloriesLabel.text = String(self.dailyCalorieGoal)
+        }
+    }
+    
+    func setConsumedCalories() {
+        let usersDB = Database.database().reference().child("Users")
+        guard let myUserID = Auth.auth().currentUser?.uid else {return}
+        let userNEW = usersDB.child(myUserID).child("ConsumedCalories")
+        userNEW.observeSingleEvent(of: .value) { (snapshot) in
+            let snapshotValue = snapshot.value as! Int
+            self.consumedCalories = snapshotValue
+            print(self.consumedCalories) //ACESS THE INT WITH THIS
+            //WRITE CODE IN HERE
+            self.caloriesConsumedLabel.text = String(self.consumedCalories)
+        }
+    }
+    
+    func setRemainingCalories() {
+        let usersDB = Database.database().reference().child("Users")
+        guard let myUserID = Auth.auth().currentUser?.uid else {return}
+        let userNEW = usersDB.child(myUserID).child("RemainingCalories")
+        userNEW.observeSingleEvent(of: .value) { (snapshot) in
+            let snapshotValue = snapshot.value as! Int
+            self.remainingCalories = snapshotValue
+            print(self.remainingCalories) //ACESS THE INT WITH THIS
+            //WRITE CODE IN HERE
+            self.remainingCaloriesLabel.text = String(self.remainingCalories)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
